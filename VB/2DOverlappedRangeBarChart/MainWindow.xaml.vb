9a02356a880e29813c140b9484954edb502c5c69
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/2DOverlappedRangeBarChart/MainWindow.xaml.vb
@@ -0,0 +1,13 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Namespace _2DOverlappedRangeBarChart
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
