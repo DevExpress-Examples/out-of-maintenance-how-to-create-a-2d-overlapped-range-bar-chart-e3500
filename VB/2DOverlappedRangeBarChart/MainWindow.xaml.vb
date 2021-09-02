71ddd4250f8e28ade29668bf3e766dfa35df764c
DevExpressExampleBot <fakeEmail>
2021-09-02 20:36:24 +0300

    autogenerate VB

--- VB/2DOverlappedRangeBarChart/MainWindow.xaml.vb
+++ VB/2DOverlappedRangeBarChart/MainWindow.xaml.vb
@@ -1,11 +1,11 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Windows
-Namespace _2DOverlappedRangeBarChart
+﻿Imports System.Windows
+Namespace _2ROverlappedRangeBarChart
 	''' <summary>
 	''' Interaction logic for MainWindow.xaml
 	''' </summary>
 	Partial Public Class MainWindow
 		Inherits Window
+
 		Public Sub New()
 			InitializeComponent()
 		End Sub
